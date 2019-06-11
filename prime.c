6cbb990781fe4ef610729ae09876b34f17658376
shivam190 <shivamsisodia36532@gmail.com>
2019-06-11 22:36:34 +0530

    prime number program in C

---
+++ prime.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num,i,flag=0;
+	printf("enter the number");
+	scanf("%d",&num);
+	for(i=2;i<=num/2;i++)
+	{
+		if(num%i==0)
+		{
+			flag=1;
+			break;
+		}
+	}
+	if(flag==1)
+	{
+		printf("1 is not a prime number");
+		
+	}
+	else{
+		if(flag==0)
+		{
+			printf("%d is a prime number",num);
+		}
+		else {
+			printf("%d is not a prime number",num);
+		}
+	}
+}
